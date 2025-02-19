db5abdcd89d15af4eb2b3d4781432a34d1c01ace
johanr001 <johris-3@student.ltu.se>
2025-02-19 11:37:57 +0100

    added some project files

---
+++ Lab4/Lab4/main.c
@@ -0,0 +1,18 @@
+/*
+ * Lab4.c
+ *
+ * Created: 19/02/2025 10:41:50
+ * Author : johan_csf2sgl
+ */ 
+
+#include <avr/io.h>
+
+
+int main(void)
+{
+    /* Replace with your application code */
+    while (1) 
+    {
+    }
+}
+
