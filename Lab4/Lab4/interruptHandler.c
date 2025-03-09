f32d16a5c24e89f4a1233aadda3f88f4fbb4980e
amadeusolofsson <coolaamadeus@gmail.com>
2025-03-09 01:17:45 +0100

    ASYNC i interrupt

--- Lab4/Lab4/interruptHandler.c
+++ Lab4/Lab4/interruptHandler.c
@@ -8,11 +8,11 @@
 #include "interruptHandler.h"
 
 int horizontal(Interrupthandler *self, int arg) {
-	SYNC(self->button, buttonCheckerLR, 0);
+	ASYNC(self->button, buttonCheckerLR, 0);
 	return 0;
 }
 
 int vertandcent(Interrupthandler *self, int arg) {
-	SYNC(self->button, buttonCheckerUDC, 0);
+	ASYNC(self->button, buttonCheckerUDC, 0);
 	return 0;
 }
