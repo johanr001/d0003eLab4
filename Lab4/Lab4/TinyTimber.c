01104b00460583f3797d9489c9f27c6cb0eb16ac
amadeusolofsson <coolaamadeus@gmail.com>
2025-03-09 02:20:49 +0100

    ?

--- Lab4/Lab4/TinyTimber.c
+++ Lab4/Lab4/TinyTimber.c
@@ -44,7 +44,7 @@
 #include <avr/interrupt.h>
 
 #define STACKSIZE       96
-#define NMSGS           15
+#define NMSGS           20
 #define NTHREADS        4
 
 #define STATUS()        (SREG & 0x80)
